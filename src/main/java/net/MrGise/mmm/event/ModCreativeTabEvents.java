da1dc7be73236f33844a3dd2054b120916ef489e
MrGise <143803114+MrGise@users.noreply.github.com>
2025-09-06 19:10:54 +0300

    added another music disc

--- src/main/java/net/MrGise/mmm/event/ModCreativeTabEvents.java
+++ src/main/java/net/MrGise/mmm/event/ModCreativeTabEvents.java
@@ -22,20 +22,11 @@ public class ModCreativeTabEvents {
 
         if (event.getTab() == ModCreativeModeTabs.RUIN_ITEMS.get()) {
             Enchantment enchantment1 = ModEnchantments.LIGHTENING_STRIKER.get();
-            Enchantment enchantment2 = ModEnchantments.GLIDE.get();
 
             for (int level = enchantment1.getMinLevel(); level <= enchantment1.getMaxLevel(); level++) {
                 // Create an enchanted book with the given enchantment and level
                 ItemStack enchantedBook = EnchantedBookItem.createForEnchantment(
                         new EnchantmentInstance(enchantment1, level));
-
-                // Add it to the creative tab
-                event.accept(enchantedBook);
-            }
-            for (int level = enchantment2.getMinLevel(); level <= enchantment2.getMaxLevel(); level++) {
-                // Create an enchanted book with the given enchantment and level
-                ItemStack enchantedBook = EnchantedBookItem.createForEnchantment(
-                        new EnchantmentInstance(enchantment2, level));
 
                 // Add it to the creative tab
                 event.accept(enchantedBook);
